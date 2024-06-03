b0a0996958f0db484eaa254a5d12417f8b43ad8f
usc-KWM007 <KWM007@student.usc.edu.au>
2024-06-03 19:53:34 +1000

    JWT cookie fix
    Got http cookies to stay after refresh by implementing secure and samesite to none, this fixed the error of the front end not accepting the cookie, now working on clearing the cookie on signout request

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
-const app = express();
 const dbController = require('./db/dbController.js');
 const { v4: uuidv4 } = require('uuid');
 const cors = require("cors")
 const { generateUUID, checkPassword, encryptPassword } = require('./db/encryption.js');
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
 app.use(cookieParser(process.env.JWT_SECRET_KEY));
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors(
   {
     origin: 'http://localhost:5173',
-    credentials: true
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Credentials",
+    ]
   }
 )); //configure to only except few urls?
 
@@ -37,6 +43,37 @@ app.get('/addTask', async (req, res) => {
   }
 }
 );
+
+app.get('/loggedIn', async (req, res) => {
+  try {
+    console.log(checkToken(req))
+    const check = checkToken(req);
+    if (check){
+      res.status(200).send('User is authenticated');
+    } else{
+      res.status(401).send('User is not authenticated');
+    }
+    
+  } catch {
+    res.status(404).send('Failed to get employees');
+  }
+}
+);
+
+app.get('/signOut', async (req, res) => {
+  try {
+    console.log(req.cookies)
+    res.clearCookie('token')
+    res.status(202).send("signed out")
+    console.log(res)
+    
+    
+  } catch {
+    res.status(404).send('Failed to sign out');
+  }
+}
+);
+
 
 app.post('/addTask', async (req, res) => {
   console.log('Got a POST request');
@@ -93,6 +130,7 @@ app.delete('/dashboard', async (req, res) => {
 
 app.get('/dashboard', async (req, res) => {
   console.log('Got a GET request');
+  console.log(req);
   try {
     const tasks = await dbController.getTasks();
     res.send(tasks);
@@ -143,13 +181,17 @@ app.post('/login', async (req, res) => {
       let employeeCredentials = await dbController.getEmployeeCredentials(data);
       //check plain password to encrypted password
       let passwordCheck = checkPassword(data.password, employeeCredentials.empid, employeeCredentials.password)
+
+
       if (passwordCheck) {
         //generate and send JWT Token
         let email = data.email;
         const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY);
-        res.cookie("token", token, { expiresIn: '1h' });
-        res.send(token);
+        //res.cookie("token", token, { httpOnly: true, expiresIn: '1h', sameSite:'strict'});
+        res.cookie("token", token, { httpOnly: true, expiresIn: '1h', sameSite: "none", secure: true });
         console.log("Someone Logged in")
+        res.status(200)
+        res.send("Logged in")
 
       } else {
         res.send("Incorrect Password")
@@ -163,9 +205,9 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/signout', async (req, res) => {
+app.get('/signOut', async (req, res) => {
   console.log('Got a GET request');
-  res.clearCookie("token");
+  res.clearCookie("token", { httpOnly: true, sameSite: "none", secure: true });
   res.send("User has signed out");
   console.log("User has signed out");
 });
